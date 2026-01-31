6a5903af0ede82971ea12b6368964cf9f721f8df
bramhankmishra <bramhankmishra12b@gmail.com>
2026-02-01 01:46:47 +0530

    google analytics and vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-import React from "react"
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import React from "react";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import "./globals.css";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Do it like a pro - Coming Soon',
-  description: 'Something extraordinary is coming. Master your craft like never before.',
-  
-}
+  title: "Do it like a pro - Coming Soon",
+  description:
+    "Something extraordinary is coming. Master your craft like never before.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`font-sans antialiased`}>
         {children}
         <Analytics />
+        <GoogleAnalytics gaId="G-ERF7M4GNKN" />
       </body>
     </html>
-  )
+  );
 }
